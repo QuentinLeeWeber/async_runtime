a75eeaef5dcadc6df0ee0c69f7204e8537d220cb
Quentin Weber <mail@quentman.de>
2026-04-12 18:29:04 +0200

    add: proper ues of waker

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{
     future::{Future, IntoFuture},
     pin::Pin,
-    sync::{Arc, Condvar, Mutex},
+    sync::{Arc, Mutex},
     task::{Context, Poll, Wake, Waker},
     time::{Duration, Instant},
 };
@@ -12,20 +12,17 @@ fn main() {
     let event_loop_handle = Arc::new(Mutex::new(EventLoopHandle::new()));
     let mut event_loop = EventLoop::new(event_loop_handle.clone());
 
-    let result = test().block_on();
-    if EXTRA_DEBUG {
-        println!("{}", result);
-    }
+    event_loop_handle.lock().unwrap().spawn(test_loop(
+        event_loop_handle.clone(),
+        String::from("test_loop 1"),
+        Duration::from_millis(0),
+    ));
 
-    event_loop_handle
-        .lock()
-        .unwrap()
-        .spawn(test_loop(event_loop_handle.clone()));
-
-    event_loop_handle
-        .lock()
-        .unwrap()
-        .spawn(test_loop2(event_loop_handle.clone()));
+    event_loop_handle.lock().unwrap().spawn(test_loop(
+        event_loop_handle.clone(),
+        String::from("test_loop 2"),
+        Duration::from_millis(1500),
+    ));
 
     println!("end");
     loop {
@@ -40,12 +37,13 @@ fn main() {
     }
 }
 
-async fn test() -> &'static str {
-    "yeet"
-}
-
-async fn test_loop(event_loop: Arc<Mutex<EventLoopHandle>>) {
+async fn test_loop(
+    event_loop: Arc<Mutex<EventLoopHandle>>,
+    text: String,
+    start_duration: Duration,
+) {
     println!("test_loop");
+    SleepFuture::new(start_duration, event_loop.clone()).await;
     loop {
         if EXTRA_DEBUG {
             println!();
@@ -54,23 +52,7 @@ async fn test_loop(event_loop: Arc<Mutex<EventLoopHandle>>) {
         let sleep = SleepFuture::new(Duration::from_secs(3), event_loop.clone());
         println!("created sleep future");
         sleep.await;
-        println!("3 sec");
-    }
-}
-
-async fn test_loop2(event_loop: Arc<Mutex<EventLoopHandle>>) {
-    SleepFuture::new(Duration::from_millis(1500), event_loop.clone()).await;
-
-    println!("test_loop");
-    loop {
-        if EXTRA_DEBUG {
-            println!();
-        }
-        println!("start loop");
-        let sleep = SleepFuture::new(Duration::from_secs(3), event_loop.clone());
-        println!("created sleep future");
-        sleep.await;
-        println!("3 sec NUMMER 2");
+        println!("3 sec {text}");
     }
 }
 
@@ -103,7 +85,7 @@ impl Future for SleepFuture {
             self.event_loop
                 .lock()
                 .unwrap()
-                .add_timer(completion_time - Instant::now(), cx.waker().clone());
+                .add_timer(completion_time, cx.waker().clone());
             self.is_spawned = true;
         }
 
@@ -122,8 +104,8 @@ impl Future for SleepFuture {
 }
 
 struct EventLoopHandle {
-    tasks: Vec<Pin<Box<dyn Future<Output = ()>>>>,
-    timers: Vec<(Duration, Waker)>,
+    tasks: Vec<(Pin<Box<dyn Future<Output = ()>>>, Arc<Signal>)>,
+    timers: Vec<(Instant, Waker)>,
 }
 
 impl EventLoopHandle {
@@ -134,22 +116,26 @@ impl EventLoopHandle {
         }
     }
 
-    fn add_timer(&mut self, duration: Duration, waker: Waker) {
-        self.timers.push((duration, waker));
+    fn add_timer(&mut self, time: Instant, waker: Waker) {
+        if EXTRA_DEBUG {
+            println!("add_timer")
+        }
+        self.timers.push((time, waker));
     }
 
     fn spawn<F>(&mut self, fut: F)
     where
         F: Future<Output = ()> + 'static,
     {
-        self.tasks.push(Box::pin(fut.into_future()));
+        let waker = Arc::new(Signal::new());
+        self.tasks.push((Box::pin(fut.into_future()), waker));
     }
 }
 
 struct EventLoop {
     handle: Arc<Mutex<EventLoopHandle>>,
-    timers: Vec<(Duration, Waker)>,
-    tasks: Vec<Pin<Box<dyn Future<Output = ()>>>>,
+    timers: Vec<(Instant, Waker)>,
+    tasks: Vec<(Pin<Box<dyn Future<Output = ()>>>, Arc<Signal>)>,
 }
 
 impl EventLoop {
@@ -168,102 +154,61 @@ impl EventLoop {
         if EXTRA_DEBUG {
             println!("update event loop");
         }
-        for (duration, waker) in self.timers.iter() {
-            if *duration <= Duration::from_secs(0) {
+
+        self.timers.retain(|(time, waker)| {
+            if *time <= Instant::now() {
                 waker.wake_by_ref();
+                false
+            } else {
+                true
             }
-        }
+        });
 
-        self.timers
-            .retain(|(duration, _)| *duration > Duration::from_secs(0));
+        self.tasks.retain_mut(|(task, signal)| {
+            if let SignalState::Waiting = *signal.state.lock().unwrap() {
+                return true;
+            }
 
-        /*for task in self.tasks.iter_mut() {
-            let waker = Waker::from(Arc::new(Signal::new()));
-            let mut context = Context::from_waker(&waker);
-            match task.as_mut().poll(&mut context) {
-                Poll::Pending => {}
-                Poll::Ready(_) => {
-                    //self.tasks.retain(|t| t != task);
+            match task
+                .as_mut()
+                .poll(&mut Context::from_waker(&Waker::from(Arc::clone(&signal))))
+            {
+                Poll::Pending => {
+                    signal.pause();
+                    true
                 }
+                Poll::Ready(_) => false,
             }
-        }*/
-        self.tasks.retain_mut(|task| {
-            task.as_mut()
-                .poll(&mut Context::from_waker(&Waker::from(Arc::new(
-                    Signal::new(),
-                ))))
-                .is_pending()
         });
     }
 }
 
-impl<F: Future> FutureExt for F {}
-pub trait FutureExt: Future {
-    fn block_on(self) -> Self::Output
-    where
-        Self: Sized,
-    {
-        let mut fut = core::pin::pin!(self.into_future());
-
-        let signal = Arc::new(Signal::new());
-
-        let waker = Waker::from(Arc::clone(&signal));
-        let mut context = Context::from_waker(&waker);
-
-        loop {
-            match fut.as_mut().poll(&mut context) {
-                Poll::Pending => signal.wait(),
-                Poll::Ready(item) => break item,
-            }
-        }
-    }
-}
-
+#[derive(Debug)]
 enum SignalState {
-    Empty,
+    Running,
     Waiting,
-    Notified,
 }
 
+#[derive(Debug)]
 struct Signal {
     state: Mutex<SignalState>,
-    cond: Condvar,
 }
 
 impl Signal {
     fn new() -> Self {
         Self {
-            state: Mutex::new(SignalState::Empty),
-            cond: Condvar::new(),
+            state: Mutex::new(SignalState::Running),
         }
     }
 
-    fn wait(&self) {
+    fn pause(&self) {
         let mut state = self.state.lock().unwrap();
-        match *state {
-            SignalState::Notified => *state = SignalState::Empty,
-            SignalState::Waiting => {
-                unreachable!("Multiple threads waiting on the same signal: Open a bug report!");
-            }
-            SignalState::Empty => {
-                *state = SignalState::Waiting;
-                while let SignalState::Waiting = *state {
-                    state = self.cond.wait(state).unwrap();
-                }
-            }
-        }
+        *state = SignalState::Waiting;
     }
 
     fn notify(&self) {
         let mut state = self.state.lock().unwrap();
-        match *state {
-            SignalState::Notified => {}
-            SignalState::Empty => *state = SignalState::Notified,
-            SignalState::Waiting => {
-                *state = SignalState::Empty;
-                self.cond.notify_one();
-            }
-        }
+        *state = SignalState::Running;
     }
 }
 
